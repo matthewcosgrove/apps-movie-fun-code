fb145e8a85eae1c59218039a3d4b75651cf1c4c7
matthewcosgrove <mcosgrove@pivotal.io>
2017-03-29 17:21:59 -0600

    Our solution to the security lab

--- applications/movie-fun-app/src/main/java/org/superbiz/moviefun/Application.java
+++ applications/movie-fun-app/src/main/java/org/superbiz/moviefun/Application.java
@@ -3,6 +3,7 @@ package org.superbiz.moviefun;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.S3ClientOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -27,11 +28,18 @@ public class Application {
     @Value("${s3.accessKey}") String s3AccessKey;
     @Value("${s3.secretKey}") String s3SecretKey;
     @Value("${s3.bucketName}") String s3BucketName;
+    @Value("${s3.endpointUrl}") String s3EndpointUrl;
 
     @Bean
     public BlobStore blobStore() {
         AWSCredentials credentials = new BasicAWSCredentials(s3AccessKey, s3SecretKey);
         AmazonS3Client s3Client = new AmazonS3Client(credentials);
+        s3Client.setEndpoint(s3EndpointUrl);
+        final S3ClientOptions clientOptions = S3ClientOptions.builder()
+                .setPathStyleAccess(true)
+                .disableChunkedEncoding()
+                .build();
+        s3Client.setS3ClientOptions(clientOptions);
 
         return new S3Store(s3Client, s3BucketName);
     }
